0018abe8fc461a745d790e09f105ab485c762860
chipslays <gorshkov34@gmail.com>
2024-12-19 10:26:25 +0400

    3.0.10-beta

--- examples/counter.php
+++ examples/counter.php
@@ -2,17 +2,39 @@
 
 use Alisa\Alisa;
 use Alisa\Context;
+use Alisa\Sessions\Session;
+use Alisa\Types\Button;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$alisa = new Alisa;
+$alisa = new Alisa([
+    'buttons' => [
+        'incrementAndDecrement' => [
+            new Button('-', 'decrement'),
+            new Button('+', 'increment'),
+        ],
+    ],
+]);
 
 $alisa->onStart(function (Context $ctx) {
-
+    $ctx->reply(
+        'Счетчик: ' . Session::get('counter', 0),
+        buttons: 'incrementAndDecrement'
+    );
 });
 
-$alisa->onAny(function (Context $ctx) {
+$alisa->onAction('increment', function (Context $ctx) {
+    $ctx->reply(
+        'Счетчик: ' . Session::increment('counter'),
+        buttons: 'incrementAndDecrement'
+    );
+});
 
+$alisa->onAction('decrement', function (Context $ctx) {
+    $ctx->reply(
+        'Счетчик: ' . Session::decrement('counter'),
+        buttons: 'incrementAndDecrement'
+    );
 });
 
 $alisa->run();
