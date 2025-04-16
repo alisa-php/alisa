0d9939b5db58fe618638d2790c7d84ad62c9fc6d
chipslays <gorshkov34@gmail.com>
2025-04-16 16:35:02 +0400

    wip

--- src/Exceptions/AlisaException.php
+++ src/Exceptions/AlisaException.php
@@ -3,17 +3,8 @@
 namespace Alisa\Exceptions;
 
 use Exception;
-use RuntimeException;
 
 class AlisaException extends Exception
 {
-    public static function invalidSceneKey()
-    {
-        return new RuntimeException('Установите корректный ключ сцены [scene.key].');
-    }
-
-    public static function invalidSceneDriver()
-    {
-        return new RuntimeException('Драйвер сцены должен быть классом сессии [scene.driver].');
-    }
+    //
 }
--- src/Stores/Middlewares.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Alisa\Stores;
-
-class Middlewares extends AbstractStore
-{
-    protected static array $items = [];
-}
